e02ae833d1af90ed62a8ed86fd6de8662efbbd23
Michail Leon De Wet <21553815@sun.ac.za>
2022-01-18 14:32:24 +0200

    - App.java > changed protected access modifiers to default. > changed class to final and access to default. > style correction. - Team.java > changed public access modifiers to default. > changed class to final and access to default. > redundancy removed in toString method. > style correction. - TeamComparator.java > changed class to final and access to default. > redundancy removed in compare method. > style correction.

--- src/main/java/com/span/challenge/Team.java
+++ src/main/java/com/span/challenge/Team.java
@@ -1,42 +1,44 @@
 package com.span.challenge;
 
-public class Team {
+final class Team {
     private String name;
     private int points;
     private int position;
 
-    public Team(String name) {
+    Team(String name) {
         this.name = name;
         this.points = 0;
         this.position = 0;
     }
 
-    public void win() {
+    void win() {
         this.points += 3;
     }
 
-    public void draw() {
+    void draw() {
         this.points += 1;
     }
 
-    public int getPoints() {
-        return Integer.valueOf(points);
+    int getPoints() {
+        return points;
     }
 
-    public String getName() {
-        return String.valueOf(name);
+    String getName() {
+        return name;
     }
 
-    public void setPosition(int position) {
+    void setPosition(int position) {
         this.position = position;
     }
 
     @Override
     public String toString() {
-        if (points == 1) {
-            return String.format("%d. %s, %d pt", position, name, points);
-        } else {
-            return String.format("%d. %s, %d pts", position, name, points);
+        String format = String.format("%d. %s, %d pt", position, name, points);
+
+        if (points != 1) {
+            return format += "s";
         }
+
+        return format;
     }
 }
